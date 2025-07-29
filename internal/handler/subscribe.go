e6c9f882e8ea5a41032578513716ad7746eb7b05
Chang lue Tsen <tension@ppanel.dev>
2025-07-29 10:13:45 -0400

    fix(handler): implement browser interception to deny access for specific user agents

--- internal/handler/subscribe.go
+++ internal/handler/subscribe.go
@@ -1,6 +1,9 @@
 package handler
 
 import (
+	"net/http"
+	"strings"
+
 	"github.com/gin-gonic/gin"
 	"github.com/perfect-panel/server/internal/logic/subscribe"
 	"github.com/perfect-panel/server/internal/svc"
@@ -17,6 +20,22 @@ func SubscribeHandler(svcCtx *svc.ServiceContext) func(c *gin.Context) {
 		}
 		req.UA = c.Request.Header.Get("User-Agent")
 		req.Flag = c.Query("flag")
+
+		// intercept browser
+		ua := c.GetHeader("User-Agent")
+		if ua == "" {
+			c.String(http.StatusForbidden, "Access denied")
+			return
+		}
+		browserKeywords := []string{"chrome", "firefox", "safari", "edge", "opera", "micromessenger"}
+		for _, keyword := range browserKeywords {
+			lcUA := strings.ToLower(ua)
+			if strings.Contains(lcUA, keyword) {
+				c.String(http.StatusForbidden, "Access denied")
+				return
+			}
+		}
+
 		l := subscribe.NewSubscribeLogic(c, svcCtx)
 		resp, err := l.Generate(&req)
 		if err != nil {
--- internal/middleware/panDomainMiddleware.go
+++ internal/middleware/panDomainMiddleware.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"net/http"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -20,6 +21,22 @@ func PanDomainMiddleware(svc *svc.ServiceContext) func(c *gin.Context) {
 				Flag:  domainArr[1],
 				UA:    c.Request.Header.Get("User-Agent"),
 			}
+
+			// intercept browser
+			ua := c.GetHeader("User-Agent")
+			if ua == "" {
+				c.String(http.StatusForbidden, "Access denied")
+				return
+			}
+			browserKeywords := []string{"chrome", "firefox", "safari", "edge", "opera", "micromessenger"}
+			for _, keyword := range browserKeywords {
+				lcUA := strings.ToLower(ua)
+				if strings.Contains(lcUA, keyword) {
+					c.String(http.StatusForbidden, "Access denied")
+					return
+				}
+			}
+
 			l := subscribe.NewSubscribeLogic(c, svc)
 			resp, err := l.Generate(&request)
 			if err != nil {
