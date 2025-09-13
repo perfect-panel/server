c1c913cb00b7ee65d166c333eae3c8e47d39af35
Chang lue Tsen <tension@ppanel.dev>
2025-09-13 14:12:33 -0400

    fix(quota): update time handling to use UnixMilli for start time in quota logic

--- internal/logic/admin/marketing/createQuotaTaskLogic.go
+++ internal/logic/admin/marketing/createQuotaTaskLogic.go
@@ -42,7 +42,7 @@ func (l *CreateQuotaTaskLogic) CreateQuotaTask(req *types.CreateQuotaTaskRequest
 		query = query.Where("`status` IN ?", []int64{0, 1, 2}) // 0: Pending 1: Active 2: Finished
 	}
 	if req.StartTime != 0 {
-		start := time.Unix(req.StartTime, 0)
+		start := time.UnixMilli(req.StartTime)
 		query = query.Where("`start_time` >= ?", start)
 	}
 	if req.EndTime != 0 {
--- internal/logic/admin/marketing/queryQuotaTaskPreCountLogic.go
+++ internal/logic/admin/marketing/queryQuotaTaskPreCountLogic.go
@@ -37,7 +37,7 @@ func (l *QueryQuotaTaskPreCountLogic) QueryQuotaTaskPreCount(req *types.QueryQuo
 		tx = tx.Where("`status` IN ?", []int64{0, 1, 2}) // 0: Pending 1: Active 2: Finished
 	}
 	if req.StartTime != 0 {
-		start := time.Unix(req.StartTime, 0)
+		start := time.UnixMilli(req.StartTime)
 		tx = tx.Where("`start_time` >= ?", start)
 	}
 	if req.EndTime != 0 {
