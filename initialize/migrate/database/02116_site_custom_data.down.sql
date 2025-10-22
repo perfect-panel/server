b2045a6e1b3719837816cc374d933948de229910
EUForest <EUForest@tutamail.com>
2025-10-22 20:36:42 +0800

    Merge remote-tracking branch 'upstream'

--- initialize/migrate/database/02116_site_custom_data.down.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `ppanel`.`system` (`category`, `key`, `value`, `type`, `desc`, `created_at`, `updated_at`)
-SELECT 'site', 'CustomData', '{
-  "kr_website_id": ""
-}', 'string', 'Custom Data', '2025-04-22 14:25:16.637', '2025-10-14 15:47:19.187'
-    WHERE NOT EXISTS (
-    SELECT 1 FROM `ppanel`.`system` WHERE `category` = 'site' AND `key` = 'CustomData'
-);
