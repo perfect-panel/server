be25048fc3db515a9a8df0483adbdae2a65e3184
Ember Moth <ember_moth@proton.me>
2026-05-20 23:06:44 +0800

    fix(auth): refresh trial caches and phone login

---
+++ internal/logic/auth/oauth/trial_cache.go
@@ -0,0 +1,27 @@
+package oauth
+
+import (
+	"context"
+
+	"github.com/perfect-panel/server/internal/model/user"
+	"github.com/perfect-panel/server/internal/svc"
+	"github.com/perfect-panel/server/pkg/logger"
+)
+
+func clearTrialSubscribeCache(ctx context.Context, svcCtx *svc.ServiceContext, trialSub *user.Subscribe) {
+	if trialSub == nil {
+		return
+	}
+	if err := svcCtx.Store.User().ClearSubscribeCache(ctx, trialSub); err != nil {
+		logger.WithContext(ctx).Errorw("ClearSubscribeCache failed",
+			logger.Field("error", err.Error()),
+			logger.Field("user_subscribe_id", trialSub.Id),
+		)
+	}
+	if err := svcCtx.Store.Subscribe().ClearCache(ctx, trialSub.SubscribeId); err != nil {
+		logger.WithContext(ctx).Errorw("Clear subscribe cache failed",
+			logger.Field("error", err.Error()),
+			logger.Field("subscribe_id", trialSub.SubscribeId),
+		)
+	}
+}
